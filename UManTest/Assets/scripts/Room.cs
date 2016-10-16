2ac6af4581f22f6e8e8d71ed0d36a05917e4c44a
ranjak <fakedarz@gmail.com>
2016-10-16 11:16:38 +0200

    Begin intro work

--- UManTest/Assets/scripts/Room.cs
+++ UManTest/Assets/scripts/Room.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Room : MonoBehaviour {
 
 	private Question[] questions;
 	private int currentQuestion = 0;
 
+	public GameObject introOutro { get; private set; }
+
 	public AudioSource voice { get; private set; }
 	public AudioSource sounds { get; private set; }
 	public AudioSource music { get; private set; }
+
+	public string introText;
+	public string outroText;
+
+	public AudioClip roomMusic;
+	public AudioClip introClip;
+	public AudioClip outroClip;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +29,13 @@ public class Room : MonoBehaviour {
 		music = GameObject.Find ("MusicSource").GetComponent<AudioSource>();
 
 		questions = GetComponentsInChildren<Question>(true);
+		introOutro = GameObject.Find("Intro");
+
+
+		if (roomMusic != null) {
+			music.clip = roomMusic;
+			music.Play ();
+		}
 
 		questions [0].gameObject.SetActive (true);
 	}
